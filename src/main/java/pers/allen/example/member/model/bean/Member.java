dc22d11e747e89dae26deefdf8dc552ea7226d3d
Allen <s890257s@yahoo.com.tw>
2023-05-09 11:06:59 +0800

    小修正

--- src/main/java/pers/allen/example/member/model/bean/Member.java
+++ src/main/java/pers/allen/example/member/model/bean/Member.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -14,7 +13,6 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import pers.allen.example.pet.model.bean.Pet;
 
